a527ef8962752b3c6246d13c0e6d4c6b287daac4
dancan <dancan.morara@student.moringaschool.com>
2025-03-22 16:16:09 +0300

    looping-along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards (names, event) {
+    let messages = [];
+    for(let i = 0; i < writeCards.length +1; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return messages
+}
+
+let msg = (writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+console.log(msg);
+
+
+let countDown = (number) => {
+    while(number >= 0 ){
+        console.log(number);
+        number --
+    }
+
+}
+
+countDown(10)
